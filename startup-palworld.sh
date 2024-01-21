91c0a871e14d29ddd6e6bbaba732c37f103c7359
palsoku <157206422+palsoku@users.noreply.github.com>
2024-01-21 20:28:57 +0900

    Update startup-palworld.sh

--- startup-palworld.sh
+++ startup-palworld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup-palworld_remove-valheim.sh をダウンロード
-curl -o setup-palworld_remove-valheim.sh https://raw.githubusercontent.com/naoya-in/Palworld-ConoHaVPS/main/setup-palworld_remove-valheim.sh
+curl -o setup-palworld_remove-valheim.sh https://raw.githubusercontent.com/palsoku/Palworld-ConoHaVPS/main/setup-palworld_remove-valheim.sh
 
 # ダウンロードしたスクリプトに実行権限を付与
 chmod +x setup-palworld_remove-valheim.sh
